4dab4dca49a7d97d70a0eb07eaf8b09ecdc58822
AngelinaKZ <150364745+AngelinaKZ@users.noreply.github.com>
2024-02-23 15:40:16 -0800

    second

--- src/main/java/org/example/test.java
+++ src/main/java/org/example/test.java
@@ -3,5 +3,6 @@ package org.example;
 public class test {
     public static void main(String[] args) {
         System.out.println("i am batch18 student");
+        System.out.println("new commit");
     }
 }
